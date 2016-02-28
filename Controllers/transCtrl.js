619ce5bc5f198b769590ba8775c233493099eca7
Marcus Wood <marcuswood@Marcuss-MacBook-Pro.local>
2016-02-27 21:28:33 -0700

    Finished up the formatting on the project, still working on the responsiveness

---
+++ Controllers/transCtrl.js
@@ -0,0 +1,18 @@
+// var Language = require('../models/translateModel.js'); 
+var config = require('../config.js')
+var googleTranslate = require('google-translate')(config.key);
+
+function translate(req, res){
+	console.log("came to ctrl")
+	googleTranslate.translate(req.body.text, req.body.languageFrom, req.body.languageTo, function(err, translation) {
+  		if(translation.translatedText.toLowerCase() === req.body.text.toLowerCase()){
+  			res.send("There doesn't seem to be a word for that")
+  		}
+  		res.send(translation.translatedText);
+	});
+	console.log("made it through google API")
+}
+
+module.exports = {
+	translate : translate
+}
---
+++ Public/javascripts/translateCtrl.js
@@ -0,0 +1,57 @@
+angular.module('translateMod')
+	.controller('translateCtrl', ['$scope', '$http', 'translateFac', function($scope, $http, translateFac){
+		$scope.greeting = "Translatr"
+
+		$scope.languages = translateFac.languages;
+		$scope.translateThis = function() {
+			console.log("function is called")
+
+			$http.post('/translate', $scope.translation)
+				.then(function(serverResponse){
+					console.log(serverResponse)
+					$scope.showText = serverResponse.data
+				})
+		}
+
+		}])
+
+angular.module('translateMod')
+	.controller('loginCtrl', ['$scope', '$http', function($scope, $http){
+		
+
+		$scope.signup = function(){
+            $http({
+                method : 'POST',
+                url    : '/signup',
+                data   : $scope.signupForm
+            }).then(function(returnData){
+                console.log(returnData)
+                if ( returnData.data.success ) { window.location.href="/quiz" }
+            })
+        }
+
+        $scope.login = function(){
+            $http({
+                method : 'POST',
+                url    : '/login_data',
+                data   : $scope.loginForm
+            }).then(function(returnData){
+                if ( returnData.data.success ) { window.location.href="/quiz" } 
+                else { console.log(returnData)}
+            })
+        }
+		
+	}])
+
+angular.module('translateMod')
+    .controller('quiz', ['$scope', '$http', function($scope, $http){
+        $http({
+            method : 'GET',
+            url    : '/api/me',
+        }).then(function(returnData){
+            console.log(returnData)
+            if ( returnData.data.user ) {
+                $scope.user = returnData.data.user
+            }
+        })        
+    }])
