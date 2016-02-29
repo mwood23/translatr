ed7d14dd1201c7cdb8265a6e5b3e1af9aa8a11bb
Marcus Wood <marcuswood@Marcuss-MacBook-Pro.local>
2016-02-29 15:24:58 -0700

    Made responsive and cleaned up some things

--- Controllers/transCtrl.js
+++ Controllers/transCtrl.js
@@ -3,14 +3,14 @@ var config = require('../config.js')
 var googleTranslate = require('google-translate')(config.key);
 
 function translate(req, res){
-	console.log("came to ctrl")
+	// console.log("came to ctrl")
 	googleTranslate.translate(req.body.text, req.body.languageFrom, req.body.languageTo, function(err, translation) {
   		if(translation.translatedText.toLowerCase() === req.body.text.toLowerCase()){
   			res.send("There doesn't seem to be a word for that")
   		}
   		res.send(translation.translatedText);
 	});
-	console.log("made it through google API")
+	// console.log("made it through google API")
 }
 
 module.exports = {
